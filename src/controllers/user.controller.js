bd06e1509056963c1c0b1e80358ffa2a385da056
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-10-14 00:47:35 +0530

    feat : Update logoutUser method to user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -138,7 +138,15 @@ const logoutUser = asyncHandler(async (req, res) => {
       new: true,
     }
   );
-  
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"));
 });
 
 // Method to generate Access and Refresh Token
