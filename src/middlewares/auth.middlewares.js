be14b354e01b9be8003626ab1cfa7cf4c156e42f
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-10-14 00:18:01 +0530

    feat : Add verifyJWT Method to auth.middlewares

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,5 +1,14 @@
+import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
 
-const verifyJWT = asyncHandler(async (req, res, next) => {});
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+});
 
 export { verifyJWT };
