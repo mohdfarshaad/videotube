7a5bb4d2a94d7c77188a5bdf4142362cf9e4b1bc
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-10-14 00:56:23 +0530

    doc : Replace  unsused res paramter to _

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
 
-const verifyJWT = asyncHandler(async (req, res, next) => {
+const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
