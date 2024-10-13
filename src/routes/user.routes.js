7841f2a31cfcbf54f3555de2ac4577872538ea3d
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-10-14 00:35:43 +0530

    feat : Add login route to user.routes from routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,23 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
 router.route("/register").post(
-  upload.fields(
-    [
-      {
-        name: "avatar",
-        maxCount: 1,
-      },
-      {
-        name: "coverImage",
-        maxCount: 1,
-      }
-    ]
-  ),
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
 
 export default router;
